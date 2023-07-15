1d0baa07fe1abfb06f2477cd04d84728140baae4
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-15 17:09:25 +0900

    comment: Message 컴포넌트 msgType props 주석 추가

--- src/components/shared/Message/index.jsx
+++ src/components/shared/Message/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { StMsg } from "./styled";
 
+// * msgType : string(error, success)
 const Message = (props) => {
     const { children, msgType = "error" } = props;
     return <StMsg msgType={msgType}>{children}</StMsg>;
