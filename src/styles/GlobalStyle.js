26547f5f850ac069f8d39d6ff4d633746d09bfa3
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-16 14:09:50 +0900

    fix: 누락된 GlobalStyle 코드 추가

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -3,6 +3,10 @@ import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
+    :root {
+        --border: 2px solid rgba(255, 255, 255, 0.5);
+        --background: rgba(250, 177, 160, 0.7);
+    }
     * {
         font-family: 'Pretendard', 'Courier New', Courier, monospace, sans-serif !important;
         font-size: 16px;
