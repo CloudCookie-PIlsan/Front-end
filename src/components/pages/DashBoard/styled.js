1dc6cd8304c59cf80b7afcbeab38ca43bb7b1ae5
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-16 13:26:30 +0900

    design: DashBoard 컴포넌트 스타일링

---
+++ src/components/pages/DashBoard/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const DashBoardContainer = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    flex-shrink: 0;
+`;
