a495351c9760ababf02c5dade3798b67a00da448
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-16 13:24:03 +0900

    design: Layout 컴포넌트 스타일링

---
+++ src/components/shared/Layout/styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const StWrapper = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #fab1a0;
+    background-image: url('../assets/images/img-heart.svg');
+    background-size: 67px 85px;
+    background-repeat: repeat;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+`;
+
+export const StContainer = styled.div`
+    width: 90%;
+    height: 80vh;
+    border: 2px solid rgba(255, 255, 255, 0.5);
+    background-color: rgba(250, 177, 160, 0.7);
+    border-radius: 10px;
+    //padding: 5rem;
+    box-sizing: border-box;
+    box-shadow: rgba(0, 0, 0, 0.09) 0px 3px 12px;
+`;
