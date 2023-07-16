d9616be8db68595d8337dcd85c80826174c95459
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-16 18:27:35 +0900

    fix: TestPage 수정

--- src/components/pages/TestPage/index.jsx
+++ src/components/pages/TestPage/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { StTextConatiner } from "./styled";
 import TestGet from "./TestGet";
+import TestPost from "./TestPost";
 
 function TestPage() {
     return (
         <StTextConatiner>
             <h1>Test Page</h1>
+            <TestPost/>
             <TestGet/>
         </StTextConatiner>
     );
