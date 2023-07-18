406ff79a3e8002bd1054ea4081bc8cf1dcadb8ba
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-18 19:26:04 +0900

    feat: SectionSent, SectionInbox 컴포넌트 추가

---
+++ src/components/pages/DashBoard/SectionInbox/index.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import Title from "../../../shared/Title";
+import SubTitle from "../../../shared/SubTitle";
+import Button from "../../../shared/Button";
+import { ContentContainer, LetterContainer, LetterBox } from "../styled";
+
+const SectionInbox = (props) => {
+    return (
+        <ContentContainer>
+            <Title>받은 쪽지함</Title>
+            <LetterContainer>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+            </LetterContainer>
+        </ContentContainer>
+    );
+};
+
+export default SectionInbox;
---
+++ src/components/pages/DashBoard/SectionSent/index.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import Title from "../../../shared/Title";
+import { ContentContainer, LetterContainer, LetterBox } from "../styled";
+
+const SectionSent = (props) => {
+    return (
+        <ContentContainer>
+            <Title>보낸 쪽지함</Title>
+            <LetterContainer>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+                <LetterBox>
+                    <h5>From. 박구름</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime laboriosam. Saepe
+                        doloribus beatae cum. Sed omnis quasi quam totam nemo id porro perspiciatis esse, mollitia
+                        molestias praesentium eaque saepe!
+                    </p>
+                </LetterBox>
+            </LetterContainer>
+        </ContentContainer>
+    );
+};
+
+export default SectionSent;
