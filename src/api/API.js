7591a71ec66eb376f8ff24e5c11b80bd3a178e19
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-18 22:17:49 +0900

    feat: 기본 API 통신 함수 구현

--- src/api/API.js
+++ src/api/API.js
@@ -1,7 +1,9 @@
 // API 통신 관련 모듈
+import { assertYieldExpression } from "@babel/types";
 import axios from "axios";
+import { async } from "q";
 
-/** axios instance */
+/** Axios instance */
 const client = axios.create({
     baseURL: `${process.env.REACT_APP_BASE_URL}`,
 });
@@ -18,4 +20,62 @@ const matchManito = async (newMatch) => {
     return response;
 };
 
-export { getManitoInfo, matchManito };
+/** 로그인 */
+const login = async (userId, password) => {
+    const response = await client.post(`/api/login`, { userId, password });
+    return response;
+};
+
+/** 회원 가입 */
+const register = async (userName, userId, password) => {
+    const response = await client.post(`/api/signup`, { userName, userId, password });
+    return response;
+};
+
+/** 마니또 정보 가져오기 */
+const fetchManitoInfo = async () => {
+    const response = await client.get(`/api/manitoes/giver`);
+    return response;
+};
+
+/** 나의 전 마니또 정보 가져오기 (전날 나를 마니또 한 사람) */
+const fetchPreviousManitoInfo = async () => {
+    const response = await client.get(`/api/manitoes/receiver`);
+    return response;
+};
+
+/** 나의 마니또 맞추기 */
+const guessManito = async (userName) => {
+    const response = await client.post(`/api/manitoes/guessManito`, { userName });
+    return response;
+};
+
+/** 쪽지 보내기 */
+const sendLetter = async (contents) => {
+    const response = await client.post(`/api/messages`, { contents });
+    return response;
+};
+
+/** 보낸 쪽지 리스트 확인 */
+const fetchSentList = async () => {
+    const response = await client.get(`/api/messages/send`);
+    return response;
+};
+
+/** 받은 쪽지 리스트 확인 */
+const fetchReceivedList = async () => {
+    const response = await client.get(`/api/messages/get`);
+    return response;
+};
+
+export {
+    getManitoInfo,
+    login,
+    fetchManitoInfo,
+    fetchPreviousManitoInfo,
+    guessManito,
+    sendLetter,
+    fetchSentList,
+    fetchReceivedList,
+    matchManito,
+};
