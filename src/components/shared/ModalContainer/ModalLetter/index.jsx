0666e2320cb20c25721af0b00b24440e9c3cf633
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-18 22:37:36 +0900

    feat: 쪽지 보내기 기능 연결

--- src/components/shared/ModalContainer/ModalLetter/index.jsx
+++ src/components/shared/ModalContainer/ModalLetter/index.jsx
@@ -4,33 +4,45 @@ import Title from "../../Title";
 import Button from "../../Button";
 import TextArea from "../../TextArea";
 import {StButtonContainer} from "../styled";
+import { useQueryClient, useMutation } from "react-query";
+import { sendLetter } from "../../../../api/API";
 
 const ModalRegister = (props) => {
     const {onClose} = props;
-    const [input, SetInput] = useState({
-        userName: "",
-        id: "",
-        pw: "",
+    const [text, SetText] = useState("");
+
+    // API 통신
+    const queryClient = useQueryClient();
+    const { mutate } = useMutation(sendLetter, {
+        onSuccess: (data) => {
+            console.log(data);
+            onClose();
+        },
+        onError: (error) => {
+            console.log(error);
+        },
     });
 
     /** 로그인 input state 처리 함수 */
     const handleInput = (e) => {
-        SetInput({
-            ...input,
-            [e.target.name]: e.target.value,
-        });
+        SetText(text);
     };
 
     const handleSubmit = () => {
-        console.log("편지 보내기!");
-        //! API 통신 붙이기
+        if(value === ""){
+            alert("쪽지 내용을 입력해주세요.");
+            return;
+        } else {
+            console.log("편지 보내기!");
+            mutate(text);
+        }
     }
 
     return (
         <ModalContainer onClose={onClose}>
             <Title>마니또에게 쪽지 보내기</Title>
             <p>이름</p>
-            <TextArea />
+            <TextArea value={text} onChange={handleInput}/>
             <StButtonContainer>
                 <Button handleBtnClick={handleSubmit}>보내기</Button>
             </StButtonContainer>
