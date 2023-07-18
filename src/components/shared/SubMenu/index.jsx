511792f4107bddc69db432d3475ae856a865e25a
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-18 19:16:46 +0900

    feat: 보낸쪽지함, 받은 쪽ㅈ함 링크 추가

--- src/components/shared/SubMenu/index.jsx
+++ src/components/shared/SubMenu/index.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import { StSubMenu, StSubMenuList } from './styled'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { StSubMenu, StSubMenuList } from "./styled";
+import { Link } from "react-router-dom";
 
 function SubMenu() {
-  return (
-    <StSubMenu>
-        <StSubMenuList>
-           <li><Link to="/dashboard/manito">마니또</Link></li> 
-           {/* <li><Link>보낸 쪽지함</Link></li>  */}
-           <li><Link to="/dashboard/inbox">받은 쪽지함</Link></li> 
-        </StSubMenuList>
-    </StSubMenu>
-  )
+    return (
+        <StSubMenu>
+            <StSubMenuList>
+                <li>
+                    <Link to="/dashboard/manito">마니또</Link>
+                </li>
+                <li>
+                    <Link to="/dashboard/sent">보낸 쪽지함</Link>
+                </li>
+                <li>
+                    <Link to="/dashboard/inbox">받은 쪽지함</Link>
+                </li>
+            </StSubMenuList>
+        </StSubMenu>
+    );
 }
 
-export default SubMenu
+export default SubMenu;
