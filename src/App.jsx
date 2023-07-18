1067b628728384696cc56b0dcc6b558add0b7bb8
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-19 01:43:54 +0900

    App.jsx 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import GlobalStyle from "./styles/GlobalStyle";
-import Layout from "./components/shared/Layout";
-import TestPage from "./components/pages/TestPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Router from "./Router";
 import { BrowserRouter } from "react-router-dom";
