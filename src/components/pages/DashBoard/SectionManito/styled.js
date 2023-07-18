c24c0ebfa5a83ff54e8147334a788aebecd46af0
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-18 12:33:37 +0900

    design: SectionManito 컴포넌트 스타일링

--- src/components/pages/DashBoard/SectionManito/styled.js
+++ src/components/pages/DashBoard/SectionManito/styled.js
@@ -1,7 +1,31 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  padding: 50px;
   `;
+
+export const StManitoWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 50px;
+  margin-top: 2rem;
+`;
+export const StManitoBox = styled.p`
+  width: 200px;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-start;
+  gap: 10px;
+  padding: 2rem;
+  border: var(--border);
+  margin-top: 1rem;
+  font-size: 1.2rem;
+  border-radius: 10px;
+  font-weight: 500;
+
+  & > span {
+    font-size: 0.8rem;
+  }
+`;
