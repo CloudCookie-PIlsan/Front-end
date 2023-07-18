aa73fd02631e35f7909255342c96d90ef31ab638
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-18 14:48:25 +0900

    design: 폰트 변경(램체)

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -7,8 +7,14 @@ const GlobalStyle = createGlobalStyle`
         --border: 2px solid rgba(255, 255, 255, 0.5);
         --background: rgba(250, 177, 160, 0.7);
     }
+    @font-face {
+    font-family: 'Ramche';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2307-1@1.1/Ramche.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+    }
     * {
-        font-family: 'Pretendard', 'Courier New', Courier, monospace, sans-serif !important;
+        font-family: 'Ramche', 'Courier New', Courier, monospace, sans-serif !important;
         font-size: 16px;
         button { border: 0; }
         select {
