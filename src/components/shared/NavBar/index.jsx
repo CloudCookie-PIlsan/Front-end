86b76373b52095a5aaafc2a6cc81e07ec23cdf3d
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-18 12:31:18 +0900

    feat: Navbar 모달 연결

--- src/components/shared/NavBar/index.jsx
+++ src/components/shared/NavBar/index.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {StNavContainer} from './styled'
 import Button from '../Button';
 import Title from '../Title';
+import ModalLogin from '../ModalContainer/ModalLogin';
+import ModalRegister from '../ModalContainer/ModalRegister';
 
-const NavBar = (props) => {
-  const {children} = props;
+const NavBar = () => {
+  const [onLogin, setOnLogin] = useState(false);
+  const [onRegister, setOnRegister] = useState(false);
+
+  const handleRegisterModal = (isOn) => {
+    setOnRegister(isOn);
+  };
+  const handleLoginModal = (isOn) => {
+    setOnLogin(isOn);
+  };
+
+
   return (
+    <>  
     <StNavContainer>
       <div>
         <Title>항해마니또💘</Title>
       </div>
       <div>
-        <Button color="danger" $negative={true}>Sign Up</Button>
-        <Button color="danger" >Log in</Button>
+        <Button color="danger" $negative={true} handleBtnClick={()=> {
+          handleRegisterModal(true)
+        }}>Sign Up</Button>
+        <Button color="danger" handleBtnClick={()=>{
+          handleLoginModal(true);
+        }}>Log in</Button>
       </div>
     </StNavContainer>
+        {onLogin && <ModalLogin onClose={()=>{
+          handleLoginModal(false)
+        }} />}
+        {onRegister && <ModalRegister onClose={()=>{
+          handleRegisterModal(false)
+        }} />}
+    </>
   )
 }
 
