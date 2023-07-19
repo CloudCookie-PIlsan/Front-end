f868faf45ceb215eae650e1ca399e35c4234f4ae
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-20 05:57:54 +0900

    design: 쪽지함 날짜 스타일링

--- src/components/shared/SubTitle/styled.js
+++ src/components/shared/SubTitle/styled.js
@@ -5,4 +5,11 @@ export const StSubTitle = styled.h2`
     line-height: 1.5;
     font-weight: 600;
     word-spacing: -0.05rem;
+
+    // 날짜 표기
+    & > span {
+        font-size: 0.8rem;
+        font-weight: 600;
+        margin-left: 0.5rem;
+    }
 `;
