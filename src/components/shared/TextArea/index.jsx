4cba056acc2986f62472e4e243a72df3fb26144d
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-20 01:50:57 +0900

    fix: TextArea 누락 props 처리

--- src/components/shared/TextArea/index.jsx
+++ src/components/shared/TextArea/index.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import {StTextArea} from './styled';
 
 const TextArea = (props) => {
-    const { name, placeholder } = props;
+    const { value, name, handleChange, placeholder } = props;
+
+    const handleOnChange = (e) => {
+      handleChange(e.target.value);
+    }
 
   return (
-    <StTextArea name={name} cols="30" rows="10" placeholder={placeholder}></StTextArea>
+    <StTextArea value={value} onChange={handleOnChange} name={name} cols="30" rows="10" placeholder={placeholder}></StTextArea>
   )
 }
 
