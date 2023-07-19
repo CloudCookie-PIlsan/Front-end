8ccef37359ceb4df7740a8034bba4027b6ef74d6
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-20 00:51:22 +0900

    feat: cookie에 만료기한 추가

--- src/modules/cookie.js
+++ src/modules/cookie.js
@@ -4,7 +4,9 @@ const cookies = new Cookies();
 
 /** 쿠키값 저장 */
 export const setCookie = (name, token) => {
-    return cookies.set(name, token);
+    const currentDate = new Date();
+    const expirationDate = new Date(currentDate.getTime() + 60 * 60 * 1000);
+    return cookies.set(name, token, { expires: expirationDate });
 };
 
 /** 쿠키값 가져오기 */
