9b3393d91183f3b7eb368695ccaa5a35567b9dab
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-20 02:32:00 +0900

    config 수정

--- src/api/API.js
+++ src/api/API.js
@@ -4,8 +4,6 @@ import axios from "axios";
 import { async } from "q";
 import { getCookie } from "../modules/cookie";
 
-const loginToken = getCookie("Authorization"); // 로그인 토큰
-
 /** Axios instance */
 const client = axios.create({
     baseURL: `${process.env.REACT_APP_BASE_URL}`,
@@ -13,11 +11,14 @@ const client = axios.create({
 });
 
 /** post config */
-const config = {
-    headers: {
-        Authorization: `Bearer ${loginToken}`,
-        "Content-type": "application/json",
-    },
+const config = () => {
+    const loginToken = getCookie("Authorization");
+    return {
+        headers: {
+            Authorization: `Bearer ${loginToken}`,
+            "Content-type": "application/json",
+        }
+    }
 }
 
 /** 로그인 */
@@ -35,42 +36,41 @@ const register = async (newData) => {
 
 /** 마니또 정보 가져오기 */
 const fetchManitoInfo = async () => {
-    const response = await client.get(`/api/manitoes/giver`, config);
+    const response = await client.get(`/api/manitoes/giver`, config());
     return response;
 };
 
 /** 나의 전 마니또 정보 가져오기 (전날 나를 마니또 한 사람) */
 const fetchPreviousManitoInfo = async () => {
-    const response = await client.get(`/api/manitoes/receiver`, config);
+    const response = await client.get(`/api/manitoes/receiver`, config());
     return response;
 };
 
 /** 나의 마니또 맞추기 */
 const guessManito = async (userName) => {
-    const response = await client.post(`/api/manitoes/guessManito`, { userName }, config);
+    const response = await client.post(`/api/manitoes/guessManito`, { userName }, config());
     return response;
 };
 
 /** 쪽지 보내기 */
 const sendLetter = async (contents) => {
-    const response = await client.post(`/api/messages`, { contents }, config);
+    const response = await client.post(`/api/messages`, { contents }, config());
     return response;
 };
 
 /** 보낸 쪽지 리스트 확인 */
 const fetchSentList = async () => {
-    const response = await client.get(`/api/messages/send`, config);
+    const response = await client.get(`/api/messages/send`, config());
     return response;
 };
 
 /** 받은 쪽지 리스트 확인 */
 const fetchReceivedList = async () => {
-    const response = await client.get(`/api/messages/get`, config);
+    const response = await client.get(`/api/messages/get`, config());
     return response;
 };
 
 export {
-    config,
     login,
     register,
     fetchManitoInfo,
