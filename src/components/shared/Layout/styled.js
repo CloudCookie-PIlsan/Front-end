1939d466821e21fe81618506930b48226a21d6fd
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-20 15:50:11 +0900

    z-index 수정

--- src/components/shared/Layout/styled.js
+++ src/components/shared/Layout/styled.js
@@ -24,4 +24,5 @@ export const StContainer = styled.div`
     background-color: rgba(255, 255, 255, 0.7);
     box-sizing: border-box;
     box-shadow: rgba(0, 0, 0, 0.09) 0px 3px 12px;
+    z-index: 100;
 `;
